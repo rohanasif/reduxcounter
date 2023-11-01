2b43f5e99a7f74bb538ad812f31b2ec420d02aa6
rohanasif1990 <rohanasif1990@gmail.com>
2023-11-01 20:12:04 +0500

    attach redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
 import countReducer from "../reducers";
 
-const store = createStore(countReducer);
+const store = createStore(
+  countReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
